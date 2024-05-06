056165d5cc687bb29ccf1852a465fbce101f039a
Ishika Rajani <ishikarajani21@gmail.com>
2024-05-06 17:15:27 +1000

    c4

--- src/main/java/sit707_tasks/DateUtil.java
+++ src/main/java/sit707_tasks/DateUtil.java
@@ -1,11 +1,7 @@
 package sit707_tasks;
 
-/**
- * @author Ahsan Habib
- */
 public class DateUtil {
 
-	// Months in order 0-11 maps to January-December.
 	private static String[] MONTHS = new String[] {
 			"January", "February", "March", "April", "May", "June", 
 			"July", "August", "September", "October", "November", "December"
@@ -40,28 +36,21 @@ public class DateUtil {
 		return year;
 	}
 	
-	/**
-	 * Increment one day.
-	 */
+	
 	public void increment() {
 		if (day < monthDuration(month, year)) {
-			// At least 1 day remaining in current month of year.
 			day++;
 		} else if (month < 12) {
-			// Last day of a month AND month is less than December, so +1d is first day of next month.
 			day = 1;
 			month++;
 		} else {
-			// Month is December, so +1d is 1st January next year.
 			day = 1;
 			month = 1;
 			year++;
 		}
 	}
 	
-	/**
-	 * Decrement one day from current date.
-	 */
+
 	public void decrement() {
 		if (day > 1) {
 			day--;
@@ -75,30 +64,20 @@ public class DateUtil {
 		}
 	}
 	
-	/**
-	 * Calculate duration of current month of year.
-	 * @param month
-	 * @param year
-	 * @return
-	 */
+	
 	public static int monthDuration(int month, int year) {		
 		if (month == 2 && year % 4 == 0) {
-			// February leap year?
 			return 29;			
 		} else if (month == 2) {  
-			// normal 28 days February
 			return 28;
 			
 		} else if (month == 4 || month == 6 || month == 9 || month == 11) {
-			// 30 days' months
 			return 30;			
 		}
-		return 31;  // rest are 31 days' months.
+		return 31;
 	}
 	
-	/**
-	 * User friendly output.
-	 */
+	
 	public String toString() {
 		return day + " " + MONTHS[month - 1] + " " + year;
 	}
